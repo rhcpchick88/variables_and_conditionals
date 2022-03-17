8caf4f3180c34d9214944733bae05c56f1344906
samantha <langridge.se@gmail.com>
2022-03-17 00:00:22 -0600

    finished script

---
+++ js/app.js
@@ -0,0 +1,15 @@
+userAge=17;
+is_subscribed=true;
+
+
+if (userAge>=18){
+    console.log("user is of age");
+} else if (userAge<18){
+    console.log("user is not of age");
+}
+
+if (is_subscribed=true){
+    console.log("user is subscribed");
+} else if (is_subscribed=false){
+    console.log("user is not subscribed");
+}
