ad0ef23e8df7766785ccc0b7b71ff0f01a6dc757
samantha <langridge.se@gmail.com>
2022-03-19 15:16:06 -0600

    Completed nesting condition in my loop and added comments

--- js/app1.js
+++ js/app1.js
@@ -2,23 +2,22 @@
 
 var userAge = [17,18,19,20];
 var isSubscribed = [true, false, true, false];
-// added array length for loop
-var arrayLength = 3;
-//created loop
-//I don't understand how to nest the conditional inside of the loop statement
-for(let counter = 0; counter <= arrayLength; counter++){
-    
-if (userAge>=18){
-    console.log("user is of age");
-} else{
-    console.log("user is not of age");
-}
 
-if (isSubscribed){
-    console.log("user is subscribed");
-} else if (!isSubscribed){
-    console.log("user is not subscribed");
-}
+//created loop indicating the loop starting count at =0 (i=0). Then indicating the condition of the loop, ending at 3. 
+//(the loop will end when the array length of 3 is reached)
+//i++ indicates that it will execute after every loop iteration, increasing the counter until 0 1 2 3 have been run through the loop.
+// the console will print the user age and their subscription status for each variable in the array order.
 
-    console.log(userAge[counter] + isSubscribed[counter]);
+for(let i=0; i <=3; i++) {
+
+    //console.log(`${userAge[i]} is ${isSubscribed[i]}`); //I commented this out after making sure it would pair my arrays correctly
+
+// making conditions for the 3 outcomes with my 2 arrays:
+    if ((userAge[i] <18) && (isSubscribed[i])) {
+        console.log('User is not of age and is subscribed'); //if user in the array is under 18 and is subscribed
+    }else if ((userAge[i] >= 18) && (isSubscribed[i])) {
+            console.log('User is of age and subscribed') //if user in the array is over 18 and is subscribed
+    }else {
+            console.log('User is of age and not subscribed') // if user in the array is over 18 and not subscribed
+    }
 }
